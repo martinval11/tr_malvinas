b535b15ee5fef01e7337e3df4337b48c4ed2cfc7
tux <tux@tux.localdomain>
2023-06-27 20:25:09 -0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import {
 	Title,
 	Tooltip,
 	Legend,
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
-import { Facebook, Instagram } from "react-bootstrap-icons";
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import { Facebook, Instagram } from 'react-bootstrap-icons';
 
-import styles from "./Home.module.css";
-import Video from "./components/Video";
+import styles from './Home.module.css';
+import Video from './components/Video';
 
 ChartJS.register(
 	BarElement,
@@ -26,30 +26,30 @@ export const options = {
 	responsive: true,
 	plugins: {
 		legend: {
-			position: "top",
+			position: 'top',
 		},
 		title: {
 			display: true,
-			text: "",
+			text: '',
 		},
 	},
 };
 
-const labels = ["Muertos", "Suicidios", "Heridos"];
+const labels = ['Muertos', 'Suicidios', 'Heridos'];
 
 export const data = {
 	labels,
 	datasets: [
 		{
-			label: "Argentina",
+			label: 'Argentina',
 			data: [649, 1877, 1657],
-			backgroundColor: "rgba(99, 198, 255, 0.5)",
+			backgroundColor: 'rgba(99, 198, 255, 0.5)',
 		},
 
 		{
-			label: "Gran Bretaña",
+			label: 'Gran Bretaña',
 			data: [255, 250, 775],
-			backgroundColor: "rgba(255, 8, 0, 0.5)",
+			backgroundColor: 'rgba(255, 8, 0, 0.5)',
 		},
 	],
 };
@@ -65,48 +65,50 @@ const App = () => {
 				<div className={styles.grid}>
 					<div className={styles.card}>
 						<h2>Discurso de Galtieri</h2>
-						<Video link="https://www.youtube.com/embed/QFp5X1KzPGU" />
+						<Video link='https://www.youtube.com/embed/QFp5X1KzPGU' />
 					</div>
 
 					<div className={styles.card}>
 						<h2>Hundimiento del Ara General Belgrano</h2>
-						<Video link="https://www.youtube.com/embed/27zg3sWk_lQ" />
+						<Video link='https://www.youtube.com/embed/27zg3sWk_lQ' />
 					</div>
 
 					<div className={styles.card}>
 						<h2>Documental de Malvinas</h2>
-						<Video link="https://www.youtube.com/embed/bNI59kQG7Ow" />
+						<Video link='https://www.youtube.com/embed/bNI59kQG7Ow' />
 					</div>
 
 					<div className={styles.card}>
 						<h2>Cortometraje de Malvinas</h2>
-						<Video link="https://www.youtube.com/embed/yW0hhriJ-Ew?start=27" />
+						<Video link='https://www.youtube.com/embed/yW0hhriJ-Ew?start=27' />
 					</div>
 
 					<div className={styles.card}>
 						<h2>Documental de la dictadura</h2>
-						<Video link="https://www.youtube.com/embed/ufzoqg3lIkY" />
+						<Video link='https://www.youtube.com/embed/ufzoqg3lIkY' />
 					</div>
 
 					<div className={styles.card}>
 						<h2>
 							Canción {'"'}No Bombardeen Buenos Aires{'"'} de Charly Garcia
 						</h2>
-						<Video link="https://www.youtube.com/embed/q7fXoJZLzY0" />
+						<Video link='https://www.youtube.com/embed/q7fXoJZLzY0' />
 					</div>
 				</div>
 
 				<section className={styles.charts}>
 					<h2>Estadísticas</h2>
-					<Bar options={options} data={data} />
+					<Bar
+						options={options}
+						data={data}
+					/>
 					<div>
 						<span>
-							Datos extraídos de{" "}
+							Datos extraídos de{' '}
 							<a
-								href="https://www.infobae.com/politica/2022/04/02/guerra-de-malvinas-el-70-de-los-combatientes-argentinos-tenia-menos-de-25-anos/"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
+								href='https://www.infobae.com/politica/2022/04/02/guerra-de-malvinas-el-70-de-los-combatientes-argentinos-tenia-menos-de-25-anos/'
+								target='_blank'
+								rel='noopener noreferrer'>
 								Infobae
 							</a>
 						</span>
@@ -115,23 +117,46 @@ const App = () => {
 
 				<div className={styles.photos}>
 					<h2>Fotos</h2>
-					<div id={styles.images} className={styles.imagesMoreHeight}>
-						<img src="img/1.webp" loading="lazy" />
-						<img src="img/2.webp" loading="lazy" />
-						<img src="img/3.webp" loading="lazy" />
+					<div
+						id={styles.images}
+						className={styles.imagesMoreHeight}>
+						<img
+							src='img/2.webp'
+							loading='lazy'
+						/>
+						<img
+							src='img/3.webp'
+							loading='lazy'
+						/>
 					</div>
 
 					<div id={styles.images}>
-						<img src="img/4.webp" loading="lazy" />
-						<img src="img/5.webp" loading="lazy" />
-						<img src="img/6.webp" loading="lazy" />
-						<img src="img/7.webp" loading="lazy" />
+						<img
+							src='img/4.webp'
+							loading='lazy'
+						/>
+						<img
+							src='img/5.webp'
+							loading='lazy'
+						/>
+						<img
+							src='img/6.webp'
+							loading='lazy'
+						/>
+						<img
+							src='img/7.webp'
+							loading='lazy'
+						/>
 					</div>
 				</div>
 
 				<div className={styles.photos}>
 					<h2>Mapa de las islas Malvinas</h2>
-					<img src="img/malvinas.webp" alt="Mapa de las islas Malvinas" loading="lazy" />
+					<img
+						src='img/malvinas.webp'
+						alt='Mapa de las islas Malvinas'
+						loading='lazy'
+					/>
 				</div>
 			</main>
 
@@ -139,17 +164,15 @@ const App = () => {
 				<h3>Redes Sociales de la EESº77</h3>
 				<div className={styles.footerLinks}>
 					<a
-						href="https://instagram.com/secundaria77mardelplata?igshid=YmMyMTA2M2Y"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
+						href='https://instagram.com/secundaria77mardelplata?igshid=YmMyMTA2M2Y'
+						target='_blank'
+						rel='noopener noreferrer'>
 						<Instagram /> Instagram
 					</a>
 					<a
-						href="https://www.facebook.com/groups/423827537724306/"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
+						href='https://www.facebook.com/groups/423827537724306/'
+						target='_blank'
+						rel='noopener noreferrer'>
 						<Facebook /> Facebook
 					</a>
 				</div>
